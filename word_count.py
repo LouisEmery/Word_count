ae7f916c35bec718d70a4c104e276e5aa617b174
LouisEmery <emerylo@educ.college-montreal.qc.ca>
2022-09-13 08:21:04 -0400

    petit code pour compter les mots, amélioré

--- word_count.py
+++ word_count.py
@@ -1,4 +1,11 @@
-def WordCount()
+#definir la fonction
+def WordCount():
+    #demande la phrase a l'utilisateur
     phrase = input("votre phrase: ")
-    print(len.split(phrase))
+    #strip la phrase pour enlever les espaces inutiles
+    phrase2 = phrase.strip(" ")
+    #split la phrae pour connaitre le nombre de mots
+    phrase3 = phrase2.split(" ")
+    #print le nombre de mots
+    print(len(phrase3))
 WordCount()
