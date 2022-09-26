6c4e8d78968011c9d6524ee96b7aefd306a87a34
emerylo <emerylo@educ.college-montreal.qc.ca>
2022-09-26 16:07:37 -0400

    petits changemments au code

--- word_count.py
+++ word_count.py
@@ -1,11 +1,19 @@
+#Louis Emery 401
+#26 septembre 2022
+
+#demande la phrase a l'utilisateur
+phrase = input(str("votre phrase: "))
+
 #definir la fonction
-def WordCount():
-    #demande la phrase a l'utilisateur
-    phrase = input("votre phrase: ")
+def count_word(phrase):
     #strip la phrase pour enlever les espaces inutiles
     phrase2 = phrase.strip(" ")
     #split la phrae pour connaitre le nombre de mots
     phrase3 = phrase2.split(" ")
-    #print le nombre de mots
-    print(len(phrase3))
-WordCount()
+    #amene le nombre de mots pour lutiliser en dehors de la phrase
+    return(len(phrase3))
+#definir la variable ndm en etant le nombre de mots definit dans la fonction
+ndm = count_word(phrase)
+
+#print le nombre de mots
+print(ndm)
